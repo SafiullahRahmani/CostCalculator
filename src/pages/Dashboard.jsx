86a5ee902b0e84711c01b9637a8424f5e1848585
Safi <80820846+SafiullahRahmani@users.noreply.github.com>
2023-04-09 14:03:45 +0200

    Teil Implementierung

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,21 @@
+// react-router-dom imports
+import { useLoaderData } from "react-router-dom";
+// helpers functions
+import { fetchData } from "../helpers";
+
+// loader
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return { userName };
+}
+const Dashboard = () => {
+  const { userName } = useLoaderData();
+  return (
+    <div>
+      <h1>{userName}</h1>
+      Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
